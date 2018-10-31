459db7fa2cabe9141e8b2e6c9cc3b482ad04d7d3
Paul <paul.hallam@gmail.com>
2018-10-31 16:16:02 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 output "public_ip" {
   description = "Public IP of the web server"
   value = "${aws_instance.webphpapp.public_ip}"
@@ -18,4 +17,3 @@ output "public_dns" {
   description = "Public DNS of the web server "
   value = "${aws_instance.webphpapp.public_dns}"
 }
->>>>>>> 4cf8f1bff5475f49f8c4e53b611239fd946e1617
