9dd0ca573115fb5ac84254d3ad51b52f749250d3
Paul <paul.hallam@gmail.com>
2018-10-31 16:00:23 +0000

    Added documentation

--- Dev2Main.tf
+++ Dev2Main.tf
@@ -1,36 +1,48 @@
+#
+# Test by going to webhost://mydb.php and mydb2.php
+#
 provider "aws" {
-  region     = "${var.TOPregion}"
+  region     = "${var.region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
 }
 
 resource "aws_instance" "database" {
-  ami           = "${lookup(var.TOPamis, var.TOPregion)}"
+# NV - Centos
+  ami = "ami-9887c6e7"
   instance_type = "t2.micro"
   associate_public_ip_address = "false"
-  vpc_security_group_ids = ["${aws_security_group.Database.id}"]
   subnet_id = "${aws_subnet.PrivateAZA.id}"
+  vpc_security_group_ids = ["${aws_security_group.database.id}"]
   key_name = "${var.aws_key_name}"
   tags {
-        Name = "database"
+    Name = "database"
+	Environment = "${var.environment}"
   }
-  user_data = <<HEREDOC
-  #!/bin/bash
-  yum update -y
-  yum install -y mysql55-server
-  service mysqld start
-  /usr/bin/mysqladmin -u root password 'secret'
-  mysql -u root -psecret -e "create user 'root'@'%' identified by 'secret';" mysql
-  mysql -u root -psecret -e 'CREATE TABLE mytable (mycol varchar(255));' test
-  mysql -u root -psecret -e "INSERT INTO mytable (mycol) values ('MyValues') ;" test
-HEREDOC
+  user_data = "${file("database.txt")}"
+}
+
+resource "aws_instance" "webphpapp" {
+# NV - Centos
+  ami = "ami-9887c6e7"
+  instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id = "${aws_subnet.PublicAZA.id}"
+  vpc_security_group_ids = ["${aws_security_group.FrontEnd.id}"]
+  key_name = "${var.aws_key_name}"
+  tags {
+    Name = "phpapp"
+	Environment = "${var.environment}"
+  }
+  user_data = "${file("webphpapp.txt")}"
 }
 
 resource "aws_vpc_dhcp_options" "mydhcp" {
-  domain_name = "${var.TOPDnsZoneName}"
+  domain_name = "${var.DnsZoneName}"
   domain_name_servers = ["AmazonProvidedDNS"]
   tags {
     Name = "My internal name"
+	Environment = "${var.environment}"
   }
 }
 
@@ -42,45 +54,18 @@ resource "aws_vpc_dhcp_options_association" "dns_resolver" {
 /* DNS PART ZONE AND RECORDS */
 
 resource "aws_route53_zone" "main" {
-  name    = "${var.TOPDnsZoneName}"
+  name    = "${var.DnsZoneName}"
   vpc_id  = "${aws_vpc.DevEc2DbVpc.id}"
   comment = "Managed by ME"
 }
 
 resource "aws_route53_record" "database" {
   zone_id = "${aws_route53_zone.main.zone_id}"
-  name = "mydatabase.${var.TOPDnsZoneName}"
+  name = "database.${var.DnsZoneName}"
   type = "A"
   ttl = "300"
   records = ["${aws_instance.database.private_ip}"]
 }
-
-resource "aws_instance" "webphpapp" {
-  ami = "${lookup(var.TOPamis, var.TOPregion)}"
-  instance_type = "t2.micro"
-  associate_public_ip_address = "true"
-  subnet_id = "${aws_subnet.PublicAZA.id}"
-  vpc_security_group_ids = ["${aws_security_group.FrontEnd.id}"]
-  key_name = "${var.aws_key_name}"
-  tags {
-        Name = "phpapp"
-  }
-
-  user_data = <<HEREDOC
-  #!/bin/bash
-  yum update -y
-  yum install -y httpd24 php56 php56-mysqlnd
-  service httpd start
-  chkconfig httpd on
-  echo "<?php" >> /var/www/html/calldb.php
-  echo "\$conn = new mysqli('database', 'root', 'secret', 'test');" >> /var/www/html/calldb.php
-  echo "\$sql = 'SELECT * FROM mytable'; " >> /var/www/html/calldb.php
-  echo "\$result = \$conn->query(\$sql); " >>  /var/www/html/calldb.php
-  echo "while(\$row = \$result->fetch_assoc()) { echo 'the value is: ' . \$row['mycol'] ;} " >> /var/www/html/calldb.php
-  echo "\$conn->close(); " >> /var/www/html/calldb.php
-  echo "?>" >> /var/www/html/calldb.php
-HEREDOC
-  }
 
 # Declare the data source
 data "aws_availability_zones" "available" {}
@@ -90,6 +75,7 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.DevEc2DbVpc.id}"
   tags {
     Name = "internet gw MY generated"
+	Environment = "${var.environment}"
   }
 }
 resource "aws_network_acl" "all" {
@@ -112,13 +98,15 @@ resource "aws_network_acl" "all" {
   }
   tags {
     Name = "open acl"
-  }
+	Environment = "${var.environment}"
+	}
 }
 
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.DevEc2DbVpc.id}"
   tags {
-      Name = "Public"
+    Name = "Public"
+	Environment = "${var.environment}"
   }
   route {
         cidr_block = "0.0.0.0/0"
@@ -129,7 +117,8 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.DevEc2DbVpc.id}"
   tags {
-      Name = "Private"
+    Name = "Private"
+	Environment = "${var.environment}"
   }
   route {
         cidr_block = "0.0.0.0/0"
@@ -144,35 +133,34 @@ resource "aws_eip" "forNat" {
 resource "aws_nat_gateway" "PublicAZA" {
   allocation_id = "${aws_eip.forNat.id}"
   subnet_id = "${aws_subnet.PublicAZA.id}"
+  depends_on = ["aws_internet_gateway.gw"]
 }
 
-####################################################
-#FROM SUBNETS.TF
-#
 resource "aws_subnet" "PublicAZA" {
   vpc_id = "${aws_vpc.DevEc2DbVpc.id}"
-  cidr_block = "${var.TOPvpcsubn-pub-cidr}"
+  cidr_block = "${var.vpcsubn-pub-cidr}"
   tags {
-        Name = "PublicAZA"
+	Name = "PublicAZA"
+	Environment = "${var.environment}"
   }
  availability_zone = "${data.aws_availability_zones.available.names[0]}"
 }
 
 resource "aws_subnet" "PrivateAZA" {
   vpc_id = "${aws_vpc.DevEc2DbVpc.id}"
-  cidr_block = "${var.TOPvpcsubn-pri-cidr}"
+  cidr_block = "${var.vpcsubn-pri-cidr}"
   tags {
-        Name = "PublicAZB"
+    Name = "PublicAZB"
+	Environment = "${var.environment}"
   }
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
 }
 
-#
-#
 resource "aws_route_table_association" "PublicAZA" {
     subnet_id = "${aws_subnet.PublicAZA.id}"
     route_table_id = "${aws_route_table.public.id}"
 }
+
 resource "aws_route_table_association" "PrivateAZA" {
     subnet_id = "${aws_subnet.PrivateAZA.id}"
     route_table_id = "${aws_route_table.private.id}"
@@ -184,6 +172,7 @@ resource "aws_security_group" "FrontEnd" {
   description = "ONLY HTTP CONNECTION INBOUND"
   tags {
     Name = "FrontEnd"
+	Environment = "${var.environment}"
 	}
   ingress {
     from_port = 80
@@ -205,12 +194,13 @@ resource "aws_security_group" "FrontEnd" {
 	}
 }
 
-resource "aws_security_group" "Database" {
-  name = "Database"
+resource "aws_security_group" "database" {
+  name = "database"
   vpc_id = "${aws_vpc.DevEc2DbVpc.id}"
   description = "ONLY tcp CONNECTION INBOUND"
   tags {
-    Name = "Database"
+    Name = "database"
+	Environment = "${var.environment}"
 	}
   ingress {
     from_port = 3306
@@ -224,6 +214,12 @@ resource "aws_security_group" "Database" {
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
 	}
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+	}
   egress {
     from_port = 0
     to_port = 0
@@ -232,13 +228,12 @@ resource "aws_security_group" "Database" {
   }
 }
 
-#
-#
 resource "aws_vpc" "DevEc2DbVpc" {
-  cidr_block = "${var.TOPvpc-cidr}"
+  cidr_block = "${var.vpc-cidr}"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags {
     Name = "DevEc2DbVpc"
+	Environment = "${var.environment}"
   }
 }
